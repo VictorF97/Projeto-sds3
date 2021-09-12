24c181d06cb8f946965d85fd3525ccd1d7c5b7a2
Victor Franca <franca1997victor@gmail.com>
2021-09-11 22:51:31 -0300

    Security config

--- frontend/src/components/BasChart/index.tsx
+++ frontend/src/components/BasChart/index.tsx
@@ -22,13 +22,13 @@ const BarChart = () => {
         ]
     };
     return (
-        <Chart 
-            options={{...options, xaxis: mockData.labels}}
+        <Chart
+            options={{ ...options, xaxis: mockData.labels }}
             series={mockData.series}
             type="bar"
             height="240"
         />
-                
+
     );
 }
 
